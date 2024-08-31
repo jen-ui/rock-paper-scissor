d64e39e19e9eaa20444fcd439a3fb8b68ebb94d3
Jenisha Khulal <jenishakhulal@gmail.com>
2024-08-31 15:24:01 +0545

    layout shift of triangle in mobile devices fixed

--- src/gamePage/components/traingle.tsx
+++ src/gamePage/components/traingle.tsx
@@ -7,27 +7,21 @@ const Triangle = () => {
  
 
   return (
-    <div className="flex bg-transparent pt-8 mx-auto w-[300px] h:[300px] md:w-[450px] md:h-[400px] relative mt-20  items-center justify-center">
-      <div className="flex ">
+    <div className="inline-flex bg-transparent pt-8 mx-auto  sm:h-[250px] sm:w-[300px] md:w-[450px] md:h-[400px] relative mt-20  items-center justify-center">
+      <div className="inline-flex ">
         <img
           src="bg-triangle.svg "
           className=" relative object-contain h-[220px] w-[250px] md:h-[280px] md:w-[300px]"
           alt="triangle"
         />
       </div>
-      <div
-       
-        className="absolute left-0 top-0 "
-      >
+      <div className="absolute left-0 top-0 ">
         <Shape index={0} />
       </div>
-      <div  className="absolute  bottom-0">
+      <div className="absolute  bottom-0">
         <Shape index={1} />
       </div>
-      <div
-       
-        className="absolute top-0 right-0"
-      >
+      <div className="absolute top-0 right-0">
         <Shape index={2} />
       </div>
     </div>
--- src/gamePage/game.tsx
+++ src/gamePage/game.tsx
@@ -17,24 +17,23 @@ export function GamePage() {
   return (
     <div className="flex items-center justify-center w-full relative">
       {displayRules && <Rules setDisplayRules={setDisplayRules} />}
-      <div className="relative bg-backgroundColor w-full min-h-screen p-12">
+      <div className="relative bg-backgroundColor  w-full min-h-screen p-12">
         <Header score={score} />
-        {
-          
-          playGame ? (
-          <ToggleComponent setScore={setScore} score={score} />
-        ) : (
-          <Triangle  />
-        )
-        }
+        <div className="flex "
+        >
+          {playGame ? (
+            <ToggleComponent setScore={setScore} score={score} />
+          ) : (
+            <Triangle />
+          )}
+        </div>
 
         <div className=" flex-col-reverse w-full gap-5   flex lg:flex-row  md:justify-between justify-center items-center mt-20">
-
-          <Button handleClick={()=>setPlayGame(!playGame)}>{ playGame?"BACK":"PLAY"}</Button>
-          <Button handleClick={()=>setDisplayRules(true)}>RULES</Button>
-
+          <Button handleClick={() => setPlayGame(!playGame)}>
+            {playGame ? "BACK" : "PLAY"}
+          </Button>
+          <Button handleClick={() => setDisplayRules(true)}>RULES</Button>
         </div>
-        
       </div>
     </div>
   );
